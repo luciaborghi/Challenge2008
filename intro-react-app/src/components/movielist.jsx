0c628ae56d66e427eec29f9fe6e2034c28e9aa0d
luciaborghi <luci_cps@hotmail.com>
2023-08-20 09:17:17 -0300

    inicializacion challenge

---
+++ intro-react-app/src/components/movielist.jsx
@@ -0,0 +1,15 @@
+//para crear la estructura de mi componente usamos:
+//usando rafce directamente declara la funcion anonima 
+import React from 'react'
+
+const movielist = () => {
+    //recibe el codigo JS
+  return (
+    <div>movielist</div>
+  )
+}
+
+export default movielist
+/*el nombre de los componentes que es lo que define
+lo exporta directramnete React 
+La forma de nombrar a los componentes es camel upercase.  
